53b72a02b9a41d82ba7a048d0c006834da4183e9
Alejandro Donoso <aledonoso@Alejandros-MacBook-Pro.local>
2019-04-12 12:35:37 +0200

    Loads anatomy of a PR correctly w/ CSS

--- inject-html-cards.js
+++ inject-html-cards.js
@@ -12,13 +12,25 @@ fetch('author-card.html')
     });
 
 
-    fetch('developer-evolution.html')
+fetch('developer-evolution.html')
     .then(function(response) {
         // When the page is loaded convert it to text
         return response.text()
     })
     .then(function(html) {
         document.querySelector('#developer-evolution-inject-point').innerHTML = html
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    });
+
+fetch('anatomy-of-pr.html')
+    .then(function(response) {
+        // When the page is loaded convert it to text
+        return response.text()
+    })
+    .then(function(html) {
+        document.querySelector('#anatomy-of-pr-inject-point').innerHTML = html
     })
     .catch(function(err) {  
         console.log('Failed to fetch page: ', err);  
