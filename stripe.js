da8500f8bd0081b7b8f364ba4386f77ca6dffc65
Alejandro Donoso <aledonoso@Alejandros-MacBook-Pro.local>
2019-04-23 15:19:57 +0200

    checkout

--- stripe.js
+++ stripe.js
@@ -11,7 +11,7 @@ var style = {
         color: '#32325d',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
-        fontSize: '16px',
+        fontSize: '14px',
         '::placeholder': {
             color: '#aab7c4'
         }
